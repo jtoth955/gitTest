7db72fe417cbb3aa013cd20561a57ce9ad8fd38f
John R Toth <tothj@oregonstate.edu>
2015-09-25 16:40:09 -0700

    I didn't like the code

--- test.cpp
+++ test.cpp
@@ -4,16 +4,7 @@ using namespace std;
 
 int main() {
 
-	cout << "This is just a test" << endl;
-
-	cout << "I like this, good move";
-
-	cout << "To make GIT work, you need to add, then commit, then push"
-
-	cout << endl;
-
-	cout << "Wah, I've got it!"
-
+	
 	return 0;
 	
 }
