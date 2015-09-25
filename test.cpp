72383c902067559b4e02d8492a9e07801334f058
John R Toth <tothj@oregonstate.edu>
2015-09-25 16:48:12 -0700

    Added some basic code:

--- test.cpp
+++ test.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
 
+	int num;
+
+	cout <<"Enter a number and I'll multiply it by 2";
+	cin  >> num;
+
+	cout << num << " times 2 is: " << num * 2 << endl;
 	
 	return 0;
 	
